214013c3409d1861cc0ca50a06d53ebf6cf21b16
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-02-25 09:48:50 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,19 @@
 const TodoNew = (props) =>{
     const {addNewToDo} = props;
-    addNewToDo("Hoa");
+    // addNewToDo("Hoa");
+    const handleClick = () =>{
+        alert("click me");
+    }
+    const handleOnChange = (name) =>{
+        console.log(">>input value", name)
+    }
     return (
         <>
         <div className="todo-new">
-          <input type="text"/>
-          <button>Add</button>
+          <input type="text" onChange={(event)=>{handleOnChange(event.target.value)}}/>
+          <button
+           style={{cursor :"pointer"}}
+           onClick={handleClick}>Add</button>
         </div>
         </>
     );
