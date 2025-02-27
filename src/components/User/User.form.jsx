11c85d28659031a8c2fa87a9d34012d3b7c8f664
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-02-27 21:18:25 +0700

    video 67

--- src/components/User/User.form.jsx
+++ src/components/User/User.form.jsx
@@ -1,27 +1,45 @@
 import { Input } from "antd";
 import { Button } from "antd";
+import { useState } from "react";
 const UserForm = () =>{
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNUmber] = useState("");
+    const handleClickBtn = () =>{
+        console.log(">>Check state: ", {fullName, email, password, phoneNumber});
+    }
     return (
         <div style={{margin: "10px 0"}}>
             <div style={{display: "flex", gap: "10px", flexDirection: "column"}}>
                 <div>
                     <span>Full name</span>
-                    <Input/>
+                    <Input
+                     value = {fullName}
+                     onChange={(e)=>{setFullName(e.target.value)}}/>
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input/>
+                    <Input
+                    value = {email}
+                    onChange={(e)=>{setEmail(e.target.value)}}/>
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password/>
+                    <Input.Password
+                     value = {password}
+                     onChange={(e)=>{setPassword(e.target.value)}}/>
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input/>
+                    <Input
+                     value = {phoneNumber}
+                     onChange={(e)=>{setPhoneNUmber(e.target.value)}}/>
                 </div>
                 <div>
-                <Button type="primary">Create User</Button>
+                <Button
+                   type="primary"
+                   onClick = {handleClickBtn} >Create User</Button>
                 </div>
             </div>
         </div>
